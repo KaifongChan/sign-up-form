3ef69afbe8fe404888df798636017b3afb4bd33d
Kaifong Chan <143081680+KaifongChan@users.noreply.github.com>
2023-09-29 19:08:05 +1000

    Added validation to ensure all 6 inputs are filled out correctly, and the passwords match each other. Otherwise, an alert pops up.

---
+++ script.jsx
@@ -0,0 +1,26 @@
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (!matchingPasswords()) {
+        alert("Passwords do not match.");
+    }
+    alert("You've successfully created an account. Please refresh.")
+});
+
+const matchingPasswords = () => {
+    let firstPassword = document.getElementById("password").value;
+    let secondPassword = document.getElementById("confirm_password").value;
+    let condition = (firstPassword === secondPassword);
+    // if (condition) {
+    //     console.log("MATCHING")
+    // }
+    // else {
+    //     console.log("NOT MATCHING");
+    // }
+    return firstPassword === secondPassword;
+}
+
+// const notMatchingPasswords = () => {
+//     let createError = document.getElement
+// }
